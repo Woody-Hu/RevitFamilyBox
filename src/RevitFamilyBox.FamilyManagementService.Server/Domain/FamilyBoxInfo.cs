a1daa4db459655538a8d5fac6bcb2f3f41b0fc1b
Stephen Hu <stephenhu@seismic.com>
2020-09-07 11:30:09 +0800

    add box domain

---
+++ src/RevitFamilyBox.FamilyManagementService.Server/Domain/FamilyBoxInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json;
+
+namespace RevitFamilyBox.FamilyManagementService.Server.Domain
+{
+    public class FamilyBoxInfo
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public JsonDocument Parameters { get; set; }
+
+        public IEnumerable<string> ParentIds { get; set; }
+
+        public FamilyBoxInfo()
+        {
+        }
+    }
+}
