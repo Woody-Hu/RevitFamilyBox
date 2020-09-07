48feaed5479b078bd07dc036afb6abc89531bccd
Stephen Hu <stephenhu@seismic.com>
2020-09-07 18:12:50 +0800

    Create IFamilyBoxInfoRepository.cs

---
+++ src/RevitFamilyBox.FamilyManagementService.Server/Domain/IFamilyBoxInfoRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace RevitFamilyBox.FamilyManagementService.Server.Domain
+{
+    public interface IFamilyBoxInfoRepository
+    {
+        Task<FamilyBoxInfo> FindFamilyBoxInfoByIdAsync(string id);
+
+        // todo describe query
+        Task<FamilyBoxInfo> FindFamilyBoxInfosAsync();
+
+        Task<FamilyBoxInfo> CreateFamilyBoxInfoAsync(FamilyBoxInfo familyBoxInfo);
+
+        Task<IEnumerable<FamilyBoxInfo>> CreateFamilyBoxInfosAsync(IEnumerable<FamilyBoxInfo> familyBoxInfos);
+
+        Task<FamilyBoxInfo> UpdateFamilyBoxInfoAsync(FamilyBoxInfo familyBoxInfo);
+
+        Task<IEnumerable<FamilyBoxInfo>> UpdateFamilyBoxInfosAsync(IEnumerable<FamilyBoxInfo> familyBoxInfos);
+
+        Task<FamilyBoxInfo> DeleteFamilyBoxInfoAsync(string id);
+
+        Task<IEnumerable<FamilyBoxInfo>> DeleteFamilyBoxInfosAsync(IEnumerable<string> ids);
+    }
+}
