239afe31b488cb0a5c952597a1b873532d639d29
Stephen Hu <stephenhu@seismic.com>
2020-09-07 12:17:04 +0800

    change to familyInfoRepository
    1. add parentIds property
    2. add add family info entity
    3. modify faimiyl info to domain
    4. add family info repository
    5. remove current family manager

---
+++ src/RevitFamilyBox.FamilyManagementService.Server/Domain/IFamilyInfoRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace RevitFamilyBox.FamilyManagementService.Server.Domain
+{
+    public interface IFamilyInfoRepository
+    {
+        Task<IEnumerable<FamilyInfo>> AddFamilyInfosAsync(IEnumerable<FamilyInfo> familyInfos);
+
+        Task<IEnumerable<FamilyInfo>> FindFamilyInfosAsync(int userId, int versionId, string xPath = null);
+
+        Task<bool> RemoveFamilyInfosAsync(IEnumerable<int> familyIds, int versionId);
+
+        Task<bool> UpdateFamilyInfosAsync(IEnumerable<FamilyInfo> familyInfos);
+    }
+}
---
+++ src/RevitFamilyBox/Config/FamilyManagerConfig.cs
@@ -0,0 +1,11 @@
+﻿namespace RevitFamilyBox.Config
+{
+  public class FamilyManagerConfig
+  {
+    public string DBConnection { get; set; }
+
+    public string DBName { get; set; }
+
+    public string DBCollection { get; set; }
+  }
+}
